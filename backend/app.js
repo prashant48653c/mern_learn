c57361943db9a74061613113eb6e862473b6a40c
Prashant <acharyaprashant227@gmail.com>
2023-09-19 15:25:18 +0545

    Started full stack project

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require("express")
+ 
+
+const app = express()
+const port=process.env.PORT || 3000;
+
+app.use(express.json())
+
+
+
+
+// -------------------------------------------------------
+
+
+app.get("/", (req, res) => {
+    res.send(`Home page`)
+})
+
+app.get("/about", (req, res) => {
+    res.send(`about page`)
+})
+
+app.get("/contact", (req, res) => {
+    res.send(`contact page`)
+})
+
+app.get("/signin", (req, res) => {
+    res.send(`signin page`)
+})
+
+app.get("/signup", (req, res) => {
+    res.send(`signup page`)
+})
+ 
+app.listen(port, () => {
+    console.log(`server started at port ${port}`);
+})
