61f371770f643e6feeb7c0d416c488c4e65fd4fa
Prashant <acharyaprashant227@gmail.com>
2023-09-20 20:40:31 +0545

    Minor changes

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcryptjs")
-const jwt=require("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 
 
 const userSchema = new mongoose.Schema({
--- backend/router/auth.js
+++ backend/router/auth.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 const User = require("../models/userSchema");
 const express = require("express")
 const bcrypt = require("bcryptjs")
-const jwt=require("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 
 
 
@@ -32,61 +32,61 @@ router.post("/register", async (req, res) => {
             res.status(400).json("Confirm the password carefully")
         }
         const existedEmail = await User.findOne({ email: email })
-      
-        if (existedEmail ) {
+
+        if (existedEmail) {
 
             res.status(400).json("Email already registered!")
         }
 
-        
 
 
-            const newUser = new User({ name, email, phone, password, cpassword, work })
-            await newUser.save()
-            res.status(201).json({messege:"Succesfully registered"})
-        
-       
+
+        const newUser = new User({ name, email, phone, password, cpassword, work })
+        await newUser.save()
+        res.status(201).json({ messege: "Succesfully registered" })
+
+
 
     } catch (error) {
         console.log(error)
-        res.status(400).json( {error:"Cannot register"})
+        res.status(400).json({ error: "Cannot register" })
     }
 })
 
 
 
 
-router.post("/signin",async (req, res) => {
-    try{
-        const {email,password}= await req.body;
-        if(!email || !password){
-            res.status(400).json({error:"Fill the data properly"})
+router.post("/signin", async (req, res) => {
+    try {
+        const { email, password } = await req.body;
+        if (!email || !password) {
+            res.status(400).json({ error: "Fill the data properly" })
         }
-        const existUser=await User.findOne({email:email})
-        const isMatch=await bcrypt.compare(password,existUser.password)
-       
-        if(!existUser){
-            res.status(404).json({ error:"Invaild Email "})
-        }
-        if(!isMatch){
-            res.status(404).json({ error:"Invaild  Password"})
+        const existUser = await User.findOne({ email: email })
+        const isMatch = await bcrypt.compare(password, existUser.password)
 
-        }else{
-            const token=await existUser.generateAuthToken()
+        if (!existUser) {
+            res.status(404).json({ error: "Invaild Email " })
+        }
+        if (!isMatch) {
+            res.status(404).json({ error: "Invaild  Password" })
+
+        } else {
+            const token = await existUser.generateAuthToken()
             console.log(token)
-            res.cookie("jwt",token,{
-                expires:new Date(Date.now() + 25892000000),
-                httpOnly:true
+            res.cookie("jwt", token, {
+                expires: new Date(Date.now() + 25892000000),
+                httpOnly: true
             })
-            
-            res.status(201).json({ messege:"Succesfully logined"})
+
+            res.status(201).json({ messege: "Succesfully logined" })
 
         }
-    }catch(err){
-        res.status(404).json( {error:"Cannot login"})
+    } catch (err) {
+        res.status(404).json({ error: "Cannot login" })
         console.log(err)
     }
- 
+
 })
 
 
