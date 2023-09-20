b04444e2b3466949d3d2804f090ec916c1b35f1c
Prashant <acharyaprashant227@gmail.com>
2023-09-20 07:36:01 +0545

    created signin route

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -12,8 +12,8 @@ const userSchema = new mongoose.Schema({
         unique: true
     },
     phone: {
-        type: Number,
-        required: false,
+        type: String,
+        required: true,
         unique: true
     },
     work: {
--- backend/router/auth.js
+++ backend/router/auth.js
@@ -1,8 +1,9 @@
+const { default: mongoose } = require("mongoose");
 const User = require("../models/userSchema");
-const express=require("express")
+const express = require("express")
 
 
-const router=express.Router()
+const router = express.Router()
 const middleware = (req, res, next) => {
     console.log("Middleware has happened")
     next()
@@ -16,17 +17,52 @@ router.get("/contact", (req, res) => {
     res.send(`contact page`)
 })
 
-router.get("/signin", (req, res) => {
-    res.send(`signin page`)
+
+
+router.post("/register", async (req, res) => {
+    try {
+        const { name, email, phone, password, cpassword, work } = req.body
+        if (!name || !email || !password || !cpassword || !work || !phone) {
+            res.status(400).send("Please fill the form carefully")
+        }
+        if (password != cpassword) {
+            res.status(400).send("Confirm the password carefully")
+        }
+        const existedEmail = await User.findOne({ email: email })
+        if (existedEmail) {
+            res.status(400).send("Email already registered!")
+        }
+        const newUser = new User({ name, email, phone, password, cpassword, work })
+
+        const result = await newUser.save()
+        res.status(201).send(result)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json( {error:"Cannot register"})
+    }
 })
 
-router.post("/register", (req, res) => {
-    
-    console.log(req.body)
-})
+router.post("/signin",async (req, res) => {
+    try{
+        const {email,password}= await req.body;
+        if(!email || !password){
+            res.status(400).json({error:"Fill the data properly"})
+        }
+        const existUser=await User.findOne({email:email, password:password})
+        if(!existUser){
+            res.status(404).json({ error:"Invaild Email or Password"})
+        }else{
+            res.status(201).json({ messege:"Succesfully logined"})
 
-
-module.exports=router
-
-
+        }
+    }catch(err){
+        res.status(404).json( {error:"Cannot login"})
+        console.log(err)
+    }
  
+})
+
+
+module.exports = router
+
+
