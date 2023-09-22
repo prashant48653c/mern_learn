cd18d279157daef5e43fb4621de0d1d91eed71f5
Prashant <acharyaprashant227@gmail.com>
2023-09-22 08:48:35 +0545

    added basic css for the application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,25 +7,26 @@ import Contact from './components/Contact'
 import Home from './components/Home'
 import Login from './components/Login'
 import Signup from './components/Signup'
+ 
 function App() {
 
   return (
     <>
-    <Router>
+  <Navbar/>
+    
       <Routes>
-      <Route  element={ <Home/> } />
+      <Route exact path='/' element={ <Home/> } />
 
-      <Route  element={ <About/> } />
+      <Route path='/about' element={ <About/> } />
 
-      <Route  element={ <Contact/> } />
-      <Route  element={ <Login/> } />
+      <Route path='/contact' element={ <Contact/> } />
+      <Route path='/login' element={ <Login/> } />
 
-      <Route  element={ <Signup/> } />
+      <Route path='/signup' element={ <Signup/> } />
 
 
       </Routes>
-    </Router>
-  <Navbar/>
+    
   
     </>
   )
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,47 +1,46 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import logo from "../assets/a.png"
 
 const Navbar = () => {
+    const navigate=useNavigate()
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">MyProfile</a>
+                    <Link className="navbar-brand" to={"/"}>
+                        <img src={logo} style={{
+                            maxWidth:"50px",
+                            borderRadius:"50%"
+                        }} alt="" />
+                    </Link>
+
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">About</a>
-                            </li>
-
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Contact</a>
+                                <Link className="nav-link" to="/about">About</Link>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Login</a>
+                                <Link className="nav-link" to="/contact">Contact</Link>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="#">SignUp</a>
+                                <Link className="nav-link" to="/login">Login</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/signup">SignUp</Link>
                             </li>
 
 
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
+                             
                            
                         </ul>
                         <form className="d-flex" role="search">
