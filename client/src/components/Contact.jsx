13a60e4942b4e8173b03f36c251b84f163efbc57
Prashant <acharyaprashant227@gmail.com>
2023-09-22 08:58:11 +0545

    Created contact us UI

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Contact = () => {
+
+function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    
+    feedback: '',
+    phoneNumber: '',
+    userGmail: '',
+    location: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can add form submission logic here
+    console.log(formData);
+  };
+
   return (
-    <div>Contact</div>
-  )
+    <div className="contact-form-container">
+      <h2>Contact Us</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Your Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+       
+        <div className="form-group">
+          <label htmlFor="feedback">Feedback</label>
+          <textarea
+            name="feedback"
+            id="feedback"
+            value={formData.feedback}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input
+            type="tel"
+            name="phoneNumber"
+            id="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="userGmail">Your Gmail (optional)</label>
+          <input
+            type="email"
+            name="userGmail"
+            id="userGmail"
+            value={formData.userGmail}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="location">Location</label>
+          <input
+            type="text"
+            name="location"
+            id="location"
+            value={formData.location}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
