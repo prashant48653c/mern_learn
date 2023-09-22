617ae0064256884f184f536f6f9f7ea9118c86dc
Prashant <acharyaprashant227@gmail.com>
2023-09-22 08:23:40 +0545

    created react routing using react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
- 
 
+import Navbar from './components/Navbar'
+import  {BrowserRouter as Router, Route ,Routes} from 'react-router-dom'
+import About from './components/About'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
 function App() {
- 
 
   return (
     <>
-  ddd
+    <Router>
+      <Routes>
+      <Route  element={ <Home/> } />
+
+      <Route  element={ <About/> } />
+
+      <Route  element={ <Contact/> } />
+      <Route  element={ <Login/> } />
+
+      <Route  element={ <Signup/> } />
+
+
+      </Routes>
+    </Router>
+  <Navbar/>
+  
     </>
   )
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,12 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <>
-
-    d
-    
-    
-    </>
+    <div>Home</div>
   )
 }
 
